dc3393d9883eb126e189c9c55e4293e167e171d3
Bilal Ahmad <bilal@xelent.pk>
2021-01-20 02:47:35 +0500

    Settings

--- resources/views/settings/includes/general.blade.php
+++ resources/views/settings/includes/general.blade.php
@@ -1,1 +1,88 @@
-General
+<div class="tile">
+    <form action="{{ route('settings.update') }}" method="POST" role="form">
+        @csrf
+        <h3 class="mb-5 py-3 border-b-2 border-indigo-700">General Settings</h3>
+
+        <div class="grid grid-cols-3 gap-2">
+            <div class="mb-3 pt-0">
+                <label class="control-label" for="site_name">Site Name</label>
+
+                <input
+                    type="text"
+                    placeholder="Enter Site name"
+                    id="site_name"
+                    name="site_name"
+                    value="{{ config('settings.site_name') }}"
+                    placeholder="Placeholder"
+                    class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
+                />
+            </div>
+
+            <div class="mb-3 pt-0">
+                <label class="control-label" for="site_title">Site Title</label>
+                <input
+                    type="text"
+                    placeholder="Enter Site title"
+                    id="site_title"
+                    name="site_title"
+                    value="{{ config('settings.site_title') }}"
+                    placeholder="Enter site title"
+                    class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
+                />
+            </div>
+
+            <div class="mb-3 pt-0">
+                <label class="control-label" for="email_address"
+                    >Email Address</label
+                >
+                <input
+                    type="text"
+                    placeholder="Enter email address"
+                    id="email_address"
+                    name="email_address"
+                    value="{{ config('settings.email_address') }}"
+                    placeholder="Enter email address"
+                    class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
+                />
+            </div>
+        </div>
+        <div class="tile-body">
+            <div class="form-group">
+                <label class="control-label" for="currency_code"
+                    >Currency Code</label
+                >
+                <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Enter store currency code"
+                    id="currency_code"
+                    name="currency_code"
+                    value="{{ config('settings.currency_code') }}"
+                />
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="currency_symbol"
+                    >Currency Symbol</label
+                >
+                <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Enter store currency symbol"
+                    id="currency_symbol"
+                    name="currency_symbol"
+                    value="{{ config('settings.currency_symbol') }}"
+                />
+            </div>
+        </div>
+        <div class="tile-footer">
+            <div class="row d-print-none mt-2">
+                <div class="col-12 text-right">
+                    <button class="btn btn-success" type="submit">
+                        <i class="fa fa-fw fa-lg fa-check-circle"></i>Update
+                        Settings
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -8,101 +8,110 @@
     @include('partials.flash')
 
     <div class="flex flex-wrap" id="tabs-id">
-  <div class="w-full">
-    <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
-      <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-        <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white bg-blue-600" onclick="changeAtiveTab(event,'tab-general')">
-          <i class="fas fa-space-shuttle text-base mr-1"></i>  General
-        </a>
-      </li>
-      <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-        <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white" onclick="changeAtiveTab(event,'tab-logo')">
-          <i class="fas fa-cog text-base mr-1"></i>  Site Logo
-        </a>
-      </li>
-      <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-        <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white" onclick="changeAtiveTab(event,'tab-footer')">
-          <i class="fas fa-briefcase text-base mr-1"></i>  Footer &amp; SEO
-        </a>
-      </li>
-      <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-        <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white" onclick="changeAtiveTab(event,'tab-social')">
-          <i class="fas fa-briefcase text-base mr-1"></i>  Social Links
-        </a>
-      </li>
-      <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-        <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white" onclick="changeAtiveTab(event,'tab-analytics')">
-          <i class="fas fa-briefcase text-base mr-1"></i>  Analytics
-        </a>
-      </li>
-      <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-        <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white" onclick="changeAtiveTab(event,'tab-payments')">
-          <i class="fas fa-briefcase text-base mr-1"></i>  Payments
-        </a>
-      </li>
-    </ul>
-    <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-      <div class="px-4 py-5 flex-auto">
-        <div class="tab-content tab-space">
-          <div class="block" id="tab-general">
-            <p>
-            @include('settings.includes.general')
-            </p>
-          </div>
-          <div class="hidden" id="tab-logo">
-            <p>
-            @include('settings.includes.logo')
-            </p>
-          </div>
-          <div class="hidden" id="tab-footer">
-            <p>
-            @include('settings.includes.footer_seo')
-            </p>
-          </div>
-          <div class="hidden" id="tab-social">
-            <p>
-            @include('settings.includes.social_links')
-            </p>
-          </div>
-          <div class="hidden" id="tab-analytics">
-            <p>
-            @include('settings.includes.analytics')
-            </p>
-          </div>
-          <div class="hidden" id="tab-payments">
-            <p>
-            @include('settings.includes.payments')
-            </p>
-          </div>
+        <div class="w-full">
+            <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a
+                        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white bg-blue-600"
+                        onclick="changeAtiveTab(event,'tab-general')"
+                    >
+                        General
+                    </a>
+                </li>
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a
+                        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white"
+                        onclick="changeAtiveTab(event,'tab-logo')"
+                    >
+                        Site Logo
+                    </a>
+                </li>
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a
+                        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white"
+                        onclick="changeAtiveTab(event,'tab-footer')"
+                    >
+                        Footer &amp; SEO
+                    </a>
+                </li>
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a
+                        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white"
+                        onclick="changeAtiveTab(event,'tab-social')"
+                    >
+                        Social Links
+                    </a>
+                </li>
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a
+                        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white"
+                        onclick="changeAtiveTab(event,'tab-analytics')"
+                    >
+                        Analytics
+                    </a>
+                </li>
+                <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a
+                        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-blue-600 bg-white"
+                        onclick="changeAtiveTab(event,'tab-payments')"
+                    >
+                        Payments
+                    </a>
+                </li>
+            </ul>
+            <div
+                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+            >
+                <div class="px-4 py-5 flex-auto">
+                    <div class="tab-content tab-space">
+                        <div class="block" id="tab-general">
+                            <p>@include('settings.includes.general')</p>
+                        </div>
+                        <div class="hidden" id="tab-logo">
+                            <p>@include('settings.includes.logo')</p>
+                        </div>
+                        <div class="hidden" id="tab-footer">
+                            <p>@include('settings.includes.footer_seo')</p>
+                        </div>
+                        <div class="hidden" id="tab-social">
+                            <p>@include('settings.includes.social_links')</p>
+                        </div>
+                        <div class="hidden" id="tab-analytics">
+                            <p>@include('settings.includes.analytics')</p>
+                        </div>
+                        <div class="hidden" id="tab-payments">
+                            <p>@include('settings.includes.payments')</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
-<script type="text/javascript">
-  function changeAtiveTab(event,tabID){
-    let element = event.target;
-    while(element.nodeName !== "A"){
-      element = element.parentNode;
-    }
-    ulElement = element.parentNode.parentNode;
-    aElements = ulElement.querySelectorAll("li > a");
-    tabContents = document.getElementById("tabs-id").querySelectorAll(".tab-content > div");
-    for(let i = 0 ; i < aElements.length; i++){
-      aElements[i].classList.remove("text-white");
-      aElements[i].classList.remove("bg-blue-600");
-      aElements[i].classList.add("text-blue-600");
-      aElements[i].classList.add("bg-white");
-      tabContents[i].classList.add("hidden");
-      tabContents[i].classList.remove("block");
-    }
-    element.classList.remove("text-blue-600");
-    element.classList.remove("bg-white");
-    element.classList.add("text-white");
-    element.classList.add("bg-blue-600");
-    document.getElementById(tabID).classList.remove("hidden");
-    document.getElementById(tabID).classList.add("block");
-  }
-</script>
-
+    <script type="text/javascript">
+        function changeAtiveTab(event, tabID) {
+            let element = event.target;
+            while (element.nodeName !== "A") {
+                element = element.parentNode;
+            }
+            ulElement = element.parentNode.parentNode;
+            aElements = ulElement.querySelectorAll("li > a");
+            tabContents = document
+                .getElementById("tabs-id")
+                .querySelectorAll(".tab-content > div");
+            for (let i = 0; i < aElements.length; i++) {
+                aElements[i].classList.remove("text-white");
+                aElements[i].classList.remove("bg-blue-600");
+                aElements[i].classList.add("text-blue-600");
+                aElements[i].classList.add("bg-white");
+                tabContents[i].classList.add("hidden");
+                tabContents[i].classList.remove("block");
+            }
+            element.classList.remove("text-blue-600");
+            element.classList.remove("bg-white");
+            element.classList.add("text-white");
+            element.classList.add("bg-blue-600");
+            document.getElementById(tabID).classList.remove("hidden");
+            document.getElementById(tabID).classList.add("block");
+        }
+    </script>
 </x-app-layout>
